e4e9dfa7f3b96cd69769837a714fedec52b5115d
p-chanmin <pc_m111@naver.com>
2023-01-16 15:00:16 +0900

    .env를 통해 설정파일 분리

--- src/api/user/controller.js
+++ src/api/user/controller.js
@@ -1,5 +1,4 @@
 const jwt = require('jsonwebtoken');
-const SECRET_KEY = "my-secret-key";
 
 /** 해당 id의 회원정보들 */
 exports.info = (ctx, next) => {
@@ -40,7 +39,7 @@ exports.login = async (ctx, next) => {
  */
 let generteToken = (payload) => {
     return new Promise((resolve, reject) => {
-        jwt.sign(payload, SECRET_KEY, (error, token) => {
+        jwt.sign(payload, process.env.APP_KEY, (error, token) => {
             if(error) { reject(error); }
             resolve(token);
         })
